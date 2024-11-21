c4412c5fff219783141684929b7a7c5e099ba527
Brittany Halterman <halterma@gmail.com>
2024-11-21 16:17:55 -0500

    Console.log exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 
 const App = () => {
+  console.log('App renders');
+
   const stories = [
     {
       title: 'React',
@@ -34,6 +36,8 @@ const App = () => {
 };
 
 const Search = () => {
+  console.log('Search renders');
+
   const [searchTerm, setSearchTerm] = React.useState('');
 
   const handleChange = (event) => {
@@ -60,23 +64,31 @@ const Search = () => {
   );
 };
 
-const List = (props) => (
-  <ul>
-    {props.list.map((item) => (
-      <Item key={item.objectID} item={item} />
-    ))}
-  </ul>
-);
+const List = (props) => {
+  console.log('List renders')
 
-const Item = (props) => (
-  <li>
-    <span>
-      <a href={props.item.url}>{props.item.title}</a>
-    </span>
-    <span>{props.item.author}</span>
-    <span>{props.item.num_comments}</span>
-    <span>{props.item.points}</span>
-  </li>
-);
+  return (
+    <ul>
+      {props.list.map((item) => (
+        <Item key={item.objectID} item={item} />
+      ))}
+    </ul>
+  )
+};
+
+const Item = (props) => {
+  console.log('Item renders');
+
+  return (
+    <li>
+      <span>
+        <a href={props.item.url}>{props.item.title}</a>
+      </span>
+      <span>{props.item.author}</span>
+      <span>{props.item.num_comments}</span>
+      <span>{props.item.points}</span>
+    </li>
+  );
+};
 
 export default App;
