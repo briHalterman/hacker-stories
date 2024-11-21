d9ed6cf16e7f09c16e1f6b9469a4cecf8b78fb32
Brittany Halterman <halterma@gmail.com>
2024-11-21 15:10:05 -0500

    Use props to pass list of items to the List component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
 import * as React from 'react';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-const App = () => (
-  <div>
-    <h1>My Hacker Stories</h1>
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
 
-    <Search />
+      <Search />
 
-    <hr />
+      <hr />
 
-    <List />
-  </div>
-);
+      <List list={stories} />
+    </div>
+  );
+};
 
 const Search = () => {
   const handleChange = (event) => {
@@ -56,9 +58,9 @@ const Search = () => {
   );
 };
 
-const List = () => (
+const List = (props) => (
   <ul>
-    {list.map((item) => (
+    {props.list.map((item) => (
       <li key={item.objectID}>
         <span>
           <a href={item.url}>{item.title}</a>
