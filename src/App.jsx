e7fa4cee9011eb7c5cf595d65ad9ec080cd6fd1b
Brittany Halterman <halterma@gmail.com>
2024-11-21 23:51:57 -0500

    Implement nested destructuring variation

--- src/App.jsx
+++ src/App.jsx
@@ -63,19 +63,17 @@ const List = ({ list }) => (
   </ul>
 );
 
-const Item = ({ item }) => {
-  // console.log('Item renders');
-
-  return (
-    <li>
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-    </li>
-  );
-};
+const Item = ({
+  item: { title, url, author, num_comments, points },
+}) => (
+  <li>
+    <span>
+      <a href={url}>{title}</a>
+    </span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
+  </li>
+);
 
 export default App;
