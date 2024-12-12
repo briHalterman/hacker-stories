6e3092ec21acc90d7d2883aea183404caa6eb9a2
Brittany Halterman <halterma@gmail.com>
2024-12-12 17:36:46 -0500

    Make list stateful by using it as initial state in React's useState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
 import * as React from 'react';
 
-const App = () => {
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
+const App = () => {
   const [searchTerm, setSearchTerm] = React.useState(
     localStorage.getItem('search') ?? 'React'
   );
+
+  const [stories, setStories] = React.useState(initialStories);
 
   React.useEffect(() => {
     console.log(searchTerm);
