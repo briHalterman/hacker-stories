c0659ed5d1db27ed22c59166b0565c2b72d9a903
Brittany Halterman <halterma@gmail.com>
2024-12-23 17:59:14 -0500

    Move story removal logic to storiesReducer for declarative state management

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,10 @@ const getAsyncStories = () =>
 const storiesReducer = (state, action) => {
   if (action.type === 'SET_STORIES') {
     return action.payload;
+  } else if (action.tyoe === 'REMOVE_STORY') {
+    return state.filter(
+      (story) => action.payload.objectID !== story.objectID
+    );
   } else {
     throw new Error();
   }
@@ -85,8 +89,8 @@ const App = () => {
 
     // setStories(newStories);
     desipatchStories({
-      type: 'SET_STORIES',
-      payload: newStories,
+      type: 'REMOVE_STORY',
+      payload: item,
     });
   };
 
