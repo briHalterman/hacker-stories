0df4321a7d1a45222e4abb4e57b318dd823836a0
Brittany Halterman <halterma@gmail.com>
2025-01-30 14:27:25 -0500

    Style SearchForm component

--- src/App.jsx
+++ src/App.jsx
@@ -139,7 +139,7 @@ const SearchForm = ({
   onSearchInput,
   onSearchSubmit,
 }) => (
-  <form onSubmit={onSearchSubmit}>
+  <form onSubmit={onSearchSubmit} className="search-form">
     <InputWithLabel
       id="search"
       value={searchTerm}
@@ -149,7 +149,11 @@ const SearchForm = ({
       <strong>Search:</strong>
     </InputWithLabel>
 
-    <button type="submit" disabled={!searchTerm}>
+    <button
+      type="submit"
+      disabled={!searchTerm}
+      className="button button_large"
+    >
       Submit
     </button>
   </form>
