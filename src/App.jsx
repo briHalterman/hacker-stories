942cced9d8d926e2c3f4be3040b14cb1ce267650
Brittany Halterman <halterma@gmail.com>
2025-01-30 17:21:11 -0500

    Refractor styling to use styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -73,6 +73,69 @@ const StyledHeadlinePrimary = styled.h1`
   letter-spacing: 2px;
 `;
 
+const StyledItem = styled.li`
+  display: flex;
+  align-items: center;
+  padding-bottom: 5px;
+`;
+
+const StyledColumn = styled.span`
+  padding: 0 5px;
+  white-space: nowrap;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+
+  a {
+    color: inherit;
+  }
+
+  width: ${(props) => props.width};
+`;
+
+const StyledButton = styled.button`
+  background: transparent;
+  border: 1px solid #171212;
+  padding: 5px;
+  cursor: pointer;
+
+  transition: all 0.1s ease-in;
+
+  &:hover {
+    background: #171212;
+    color: #ffffff;
+  }
+`;
+
+const StyledButtonSmall = styled(StyledButton)`
+  padding: 5px;
+`;
+
+const StyledButtonLarge = styled(StyledButton)`
+  padding: 10px;
+`;
+
+const StyledSearchForm = styled.form`
+  padding: 10px 0 20px 0;
+  display: flex;
+  align-items: baseline;
+`;
+
+const StyledLabel = styled.label`
+  border-top: 1px solid #171212;
+  border-left: 1px solid #171212;
+  padding-left: 5px;
+  font-size: 24px;
+`;
+
+const StyledInput = styled.input`
+  border: none;
+  border-botttom: 1px solid #171212;
+  background-color: transparent;
+
+  font-size: 24px;
+`;
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useStorageState(
     'search',
@@ -158,7 +221,7 @@ const SearchForm = ({
   onSearchSubmit,
   className,
 }) => (
-  <form onSubmit={onSearchSubmit} className={styles.searchForm}>
+  <StyledSearchForm onSubmit={onSearchSubmit}>
     <InputWithLabel
       id="search"
       value={searchTerm}
@@ -168,17 +231,17 @@ const SearchForm = ({
       <strong>Search:</strong>
     </InputWithLabel>
 
-    <button
+    <StyledButtonLarge
       type="submit"
       disabled={!searchTerm}
-      className={clsx(styles.button, styles.buttonLarge)}
+      // className={clsx(styles.button, styles.buttonLarge)}
       // className={clsx(styles.button, {
       //   [styles.buttonLarge]: isLarge,
       // })}
     >
       Submit
-    </button>
-  </form>
+    </StyledButtonLarge>
+  </StyledSearchForm>
 );
 
 const InputWithLabel = ({
@@ -202,19 +265,17 @@ const InputWithLabel = ({
 
   return (
     <>
-      <label htmlFor={id} className={styles.label}>
-        {children}
-      </label>
+      <StyledLabel htmlFor={id}>{children}</StyledLabel>
       &nbsp;
       {/* Pass ref to element's JSX-reserved ref attribute */}
-      <input
+      <StyledInput
         ref={inputRef}
         id={id}
         type={type}
         value={value}
         // autoFocus={isFocused}
         onChange={onInputChange}
-        className={styles.input}
+        // className={styles.input}
       />
     </>
   );
@@ -236,23 +297,23 @@ const List = ({ list, onRemoveItem }) => (
 );
 
 const Item = ({ item, onRemoveItem }) => (
-  <li className={styles.item}>
-    <span style={{ width: '40%' }}>
+  <StyledItem>
+    <StyledColumn width="40%">
       <a href={item.url}>{item.title}</a>
-    </span>
-    <span style={{ width: '30%' }}>{item.author}</span>
-    <span style={{ width: '10%' }}>{item.num_comments}</span>
-    <span style={{ width: '10%' }}>{item.points}</span>
-    <span style={{ width: '10%' }}>
-      <button
+    </StyledColumn>
+    <StyledColumn width="30%">{item.author}</StyledColumn>
+    <StyledColumn width="10%">{item.num_comments}</StyledColumn>
+    <StyledColumn width="10%">{item.points}</StyledColumn>
+    <StyledColumn width="10%">
+      <StyledButtonSmall
         type="button"
         onClick={() => onRemoveItem(item)}
         className={`${styles.button} ${styles.buttonSmall}`}
       >
         Dismiss
-      </button>
-    </span>
-  </li>
+      </StyledButtonSmall>
+    </StyledColumn>
+  </StyledItem>
 );
 
 export default App;
