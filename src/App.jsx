21d570f7f645aa8d1fc83cc37d504dd1071e3c2e
Brittany Halterman <halterma@gmail.com>
2025-02-02 20:56:54 -0500

    Apply performance optimization techniques to prevent each Item from re-rendering

--- src/App.jsx
+++ src/App.jsx
@@ -351,24 +351,28 @@ const List = React.memo(
     )
 );
 
-const Item = ({ item, onRemoveItem }) => (
-  <StyledItem>
-    <StyledColumn width="40%">
-      <a href={item.url}>{item.title}</a>
-    </StyledColumn>
-    <StyledColumn width="30%">{item.author}</StyledColumn>
-    <StyledColumn width="10%">{item.num_comments}</StyledColumn>
-    <StyledColumn width="10%">{item.points}</StyledColumn>
-    <StyledColumn width="10%">
-      <StyledButtonSmall
-        type="button"
-        onClick={() => onRemoveItem(item)}
-        // className={`${styles.button} ${styles.buttonSmall}`}
-      >
-        <AiOutlineCheck height="18px" width="18px" color="green" />
-      </StyledButtonSmall>
-    </StyledColumn>
-  </StyledItem>
-);
+const Item = React.memo(({ item, onRemoveItem }) => {
+  console.log('E');
+
+  return (
+    <StyledItem>
+      <StyledColumn width="40%">
+        <a href={item.url}>{item.title}</a>
+      </StyledColumn>
+      <StyledColumn width="30%">{item.author}</StyledColumn>
+      <StyledColumn width="10%">{item.num_comments}</StyledColumn>
+      <StyledColumn width="10%">{item.points}</StyledColumn>
+      <StyledColumn width="10%">
+        <StyledButtonSmall
+          type="button"
+          onClick={() => onRemoveItem(item)}
+          // className={`${styles.button} ${styles.buttonSmall}`}
+        >
+          <AiOutlineCheck height="18px" width="18px" color="green" />
+        </StyledButtonSmall>
+      </StyledColumn>
+    </StyledItem>
+  );
+});
 
 export default App;
