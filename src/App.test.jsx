7b01740887e059a5d50a1e1bc17c239544447a5a
Brittany Halterman <halterma@gmail.com>
2025-02-17 17:31:38 -0500

    Add test for "More" button functionality

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -578,6 +578,52 @@ describe('App', () => {
     );
   });
 
+  it('loads more stories when clicking the "More" button', async () => {
+    const intitialStories = [storyOne, storyTwo];
+
+    const nextPageStories = [
+      {
+        title: 'Node.js',
+        url: 'nodejs.org/en',
+        author: 'Ryan Dahl',
+        num_comments: 10,
+        points: 15,
+        objectID: 3,
+      },
+    ];
+
+    const firstPagePromise = Promise.resolve({
+      data: { hits: intitialStories, page: 0 },
+    });
+
+    const secondPagePromise = Promise.resolve({
+      data: { hits: nextPageStories, page: 1 },
+    });
+
+    axios.get.mockImplementation((url) => {
+      if (url.includes('page=0')) return firstPagePromise;
+      if (url.includes('page=1')) return secondPagePromise;
+      return Promise.reject();
+    });
+
+    render(<App />);
+
+    await waitFor(() =>
+      expect(screen.getByText('React')).toBeInTheDocument()
+    );
+
+    const moreButton = screen.getByRole('button', { name: 'More' });
+    expect(moreButton).toBeInTheDocument();
+
+    fireEvent.click(moreButton);
+
+    await waitFor(() =>
+      expect(screen.getByText('Node.js')).toBeInTheDocument()
+    );
+
+    expect(screen.getByText('Ryan Dahl')).toBeInTheDocument();
+  });
+
   it('renders snapshot', async () => {
     const promise = Promise.resolve({
       data: {
